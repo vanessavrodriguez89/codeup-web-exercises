5aed0f4064f5a8108690feb0fff1c2f8b5f54356
Vanessa Rodriguez <vanessa.v.rodriguez89@gmail.com>
2020-06-30 10:20:58 -0500

    loops exercises

--- js/while.js
+++ js/while.js
@@ -26,7 +26,8 @@ while (startingNum <= 32768) {
 }
 
 //* An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is a way get the random numbers for this exercise.*/
-var allCones = Math.floor(Math.random() * 50) + 50;
+
+let allCones = Math.floor(Math.random() * 50) + 50;
 let amountPurchased;
 do {
     console.log("I have " + allCones + " for sale.")
@@ -38,5 +39,6 @@ do {
         console.log("Yay! I sold them all!");
     } else {
         console.log("Cannot sell you " + amountPurchased + " cones. I only have " + allCones);
+        // break;
     }
-} while (true);
+} while (allCones !== 0);
